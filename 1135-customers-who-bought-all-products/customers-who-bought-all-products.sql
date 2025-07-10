3f861117882bce87a256281284bcbd01d5129bba
Mudit Singh <singhmudit8787@gmail.com>
2025-07-10 22:26:18 +0530

    Time: 597 ms (33.42%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select c.customer_id
+from Customer as c
+join Product as p 
+group by customer_id
+having count( distinct c.product_key)=count( distinct p.product_key)
